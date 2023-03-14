522f96c9c4baf6abdefd07d5e70708f9327ff21e
Gabe Hassler <ghassler@rand.org>
2023-03-14 11:46:28 -0700

    perimter information in adjacency

--- src/dr/evomodel/territorydesign/ContiguityStatistic.java
+++ src/dr/evomodel/territorydesign/ContiguityStatistic.java
@@ -3,62 +3,145 @@ package dr.evomodel.territorydesign;
 import dr.evolution.tree.NodeRef;
 import dr.evomodel.tree.TreeModel;
 import dr.inference.model.*;
+import dr.math.distributions.Distribution;
 import dr.xml.*;
 
 import java.util.ArrayList;
 
-public class ContiguityStatistic extends Statistic.Abstract {
+public class ContiguityStatistic extends AbstractModelLikelihood {
 
     private final TaxaAdjacencyMatrix adjacencyMatrix;
     private final Parameter threshold;
+    private int nDiscongiguities = 0;
+    private double[] areas;
+    private double[] perimeters;
+    private double[] groupSizes;
+    //    private final Distribution groupSizeDistribution;
+    private final Distribution perimeterDistribution;
+    private final double discontiguityPenalty;
 
-    public ContiguityStatistic(TaxaAdjacencyMatrix adjacencyMatrix, Parameter threshold) {
+    public ContiguityStatistic(TaxaAdjacencyMatrix adjacencyMatrix,
+                               Parameter threshold,
+//                               Distribution groupSizeDistribution,
+                               Distribution perimeterDistribution,
+                               double discontiguityPenalty) {
+        super(CONTIGUITY_LIKELIHOOD);
         this.adjacencyMatrix = adjacencyMatrix;
         this.threshold = threshold;
+//        this.groupSizeDistribution = groupSizeDistribution;
+        this.perimeterDistribution = perimeterDistribution;
+        this.discontiguityPenalty = discontiguityPenalty;
     }
 
-    private int countDiscontiguities() {
+    private void update() {
         TreeModel treeModel = adjacencyMatrix.getTreeModel();
-        AdjacencyAccumulator accumulator = countDiscontiguities(treeModel, treeModel.getRoot());
-        return accumulator.nDiscongiguities;
+        ArrayList<AdjacencyAccumulator> accumulators = countDiscontiguities(treeModel, treeModel.getRoot());
+        int n = accumulators.size();
+        this.perimeters = new double[n];
+        this.areas = new double[n];
+        this.groupSizes = new double[n];
+        this.nDiscongiguities = 0;
+
+        for (int i = 0; i < n; i++) {
+            AdjacencyAccumulator accumulator = accumulators.get(i);
+            ArrayList<Integer> taxa = accumulator.descendants;
+            int ni = taxa.size();
+            groupSizes[i] = ni;
+            this.nDiscongiguities += accumulator.nDiscongiguities;
+            for (int j = 0; j < ni; j++) {
+                areas[i] += adjacencyMatrix.getArea(j);
+                perimeters[i] += adjacencyMatrix.getPerimeter(j);
+                for (int k = (j + 1); k < ni; k++) {
+                    perimeters[i] -= 2 * adjacencyMatrix.getSharedPerimeter(j, k);
+                }
+            }
+        }
+
     }
-    private AdjacencyAccumulator countDiscontiguities(TreeModel treeModel, NodeRef node) {
+
+    private ArrayList<AdjacencyAccumulator> countDiscontiguities(TreeModel treeModel, NodeRef node) {
         ArrayList<Integer> descendants = new ArrayList<>();
+        ArrayList<AdjacencyAccumulator> accumulators = new ArrayList<>();
+
         if (treeModel.getChildCount(node) == 0) {
             descendants.add(node.getNumber());
-            return new AdjacencyAccumulator(0, descendants);
+            accumulators.add(new AdjacencyAccumulator(0, descendants));
+            return accumulators;
         }
-        AdjacencyAccumulator a0 = countDiscontiguities(treeModel, treeModel.getChild(node, 0));
-        AdjacencyAccumulator a1 = countDiscontiguities(treeModel, treeModel.getChild(node, 1));
+        ArrayList<AdjacencyAccumulator> as0 = countDiscontiguities(treeModel, treeModel.getChild(node, 0));
+        ArrayList<AdjacencyAccumulator> as1 = countDiscontiguities(treeModel, treeModel.getChild(node, 1));
+
+        if (treeModel.getNodeHeight(node) > threshold.getParameterValue(0)) {
+            accumulators.addAll(as0);
+            accumulators.addAll(as1);
+            return accumulators;
+        }
+
+        AdjacencyAccumulator a0 = as0.get(0);
+        AdjacencyAccumulator a1 = as1.get(0);
 
         descendants.addAll(a0.descendants);
         descendants.addAll(a1.descendants);
         int n = a0.nDiscongiguities + a1.nDiscongiguities;
 
-        if (treeModel.getNodeHeight(node) < threshold.getParameterValue(0)) {
-            for (int i0 = 0; i0 < a0.descendants.size(); i0++) {
-                int taxon0 = a0.descendants.get(i0);
-                for (int i1 = 0; i1 < a1.descendants.size(); i1++) {
-                    int taxon1 = a1.descendants.get(i1);
-                    if (adjacencyMatrix.areAdjacent(taxon0, taxon1)) {
-                        return new AdjacencyAccumulator(n, descendants);
-                    }
+        for (int i0 = 0; i0 < a0.descendants.size(); i0++) {
+            int taxon0 = a0.descendants.get(i0);
+            for (int i1 = 0; i1 < a1.descendants.size(); i1++) {
+                int taxon1 = a1.descendants.get(i1);
+                if (adjacencyMatrix.areAdjacent(taxon0, taxon1)) {
+                    accumulators.add(new AdjacencyAccumulator(n, descendants));
+                    return accumulators;
                 }
             }
-            return new AdjacencyAccumulator(n + 1, descendants);
         }
-
-        return new AdjacencyAccumulator(n, descendants);
+        accumulators.add(new AdjacencyAccumulator(n + 1, descendants));
+        return accumulators;
     }
 
     @Override
-    public int getDimension() {
-        return 1;
+    protected void handleModelChangedEvent(Model model, Object object, int index) {
+        // do nothing
     }
 
     @Override
-    public double getStatisticValue(int dim) {
-        return countDiscontiguities();
+    protected void handleVariableChangedEvent(Variable variable, int index, Parameter.ChangeType type) {
+        // do nothing
+    }
+
+    @Override
+    protected void storeState() {
+        // do nothing
+    }
+
+    @Override
+    protected void restoreState() {
+        // do nothing
+    }
+
+    @Override
+    protected void acceptState() {
+        // do nothing
+    }
+
+    @Override
+    public Model getModel() {
+        return this;
+    }
+
+    @Override
+    public double getLogLikelihood() {
+        update();
+        double ll = -discontiguityPenalty * nDiscongiguities;
+        for (int i = 0; i < areas.length; i++) {
+            double ratio = perimeters[i] * perimeters[i] / areas[i];
+            ll += perimeterDistribution.logPdf(ratio) * groupSizes[i];
+        }
+        return ll;
+    }
+
+    @Override
+    public void makeDirty() {
+        // do nothing
     }
 
     private static class AdjacencyAccumulator {
@@ -74,22 +157,31 @@ public class ContiguityStatistic extends Statistic.Abstract {
 
 
     private static String THRESHOLD = "threshold";
+    private static String PENALTY = "discontiguityPenalty";
+    private static String RATIO_DISTRIBUTION = "perimeterAreaRatioDistribution";
+    private static String CONTIGUITY_LIKELIHOOD = "contiguityLikelihood";
 
     public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
         @Override
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
             TaxaAdjacencyMatrix adjacencyMatrix = (TaxaAdjacencyMatrix) xo.getChild(TaxaAdjacencyMatrix.class);
             Parameter threshold = (Parameter) xo.getChild(THRESHOLD).getChild(Parameter.class);
-            return new ContiguityStatistic(adjacencyMatrix, threshold);
+            Distribution ratioDistribution = (Distribution) xo.getChild(RATIO_DISTRIBUTION).getChild(Distribution.class);
+            double penalty = xo.getDoubleAttribute(PENALTY);
+            return new ContiguityStatistic(adjacencyMatrix, threshold, ratioDistribution, penalty);
         }
 
         @Override
         public XMLSyntaxRule[] getSyntaxRules() {
             return new XMLSyntaxRule[]{
                     new ElementRule(TaxaAdjacencyMatrix.class),
-                    new ElementRule(THRESHOLD, new XMLSyntaxRule[] {
+                    new ElementRule(THRESHOLD, new XMLSyntaxRule[]{
                             new ElementRule(Parameter.class)
-                    })
+                    }),
+                    new ElementRule(RATIO_DISTRIBUTION, new XMLSyntaxRule[]{
+                            new ElementRule(Distribution.class)
+                    }),
+                    AttributeRule.newDoubleRule(PENALTY)
             };
         }
 
@@ -105,7 +197,7 @@ public class ContiguityStatistic extends Statistic.Abstract {
 
         @Override
         public String getParserName() {
-            return "contiguityStatistic";
+            return CONTIGUITY_LIKELIHOOD;
         }
     };
 }
--- src/dr/evomodel/territorydesign/TaxaAdjacencyMatrix.java
+++ src/dr/evomodel/territorydesign/TaxaAdjacencyMatrix.java
@@ -11,15 +11,34 @@ public class TaxaAdjacencyMatrix {
     private final TreeModel treeModel;
     private final int[][] adjacency;
     private final double[][] sharedBorder;
+    private final double[] perimeter;
+    private final double[] area;
 
-    public TaxaAdjacencyMatrix(TreeModel treeModel, int[][] adjacency, double[][] sharedBorder) {
+    public TaxaAdjacencyMatrix(TreeModel treeModel,
+                               int[][] adjacency,
+                               double[][] sharedBorder,
+                               double[] perimeter,
+                               double[] area) {
         this.treeModel = treeModel;
         this.adjacency = adjacency;
         this.sharedBorder = sharedBorder;
+        this.perimeter = perimeter;
+        this.area = area;
     }
 
     public boolean areAdjacent(int i, int j) {
         return adjacency[i][j] == 1;
+    }
+    public double getPerimeter(int i) {
+        return perimeter[i];
+    }
+
+    public double getArea(int i) {
+        return area[i];
+    }
+
+    public double getSharedPerimeter(int i, int j) {
+        return sharedBorder[i][j];
     }
 
     public TreeModel getTreeModel() {
@@ -27,8 +46,10 @@ public class TaxaAdjacencyMatrix {
     }
 
     private static final String ADJACENCY = "adjacency";
-    private static final String SHARED_BORDER = "sharedBorder";
-
+    private static final String SHARED_BORDER = "sharedPerimeter";
+    private static final String PERIMETER = "perimeter";
+    private static final String AREA = "area";
+    private static final String TAXON_DATA = "taxonData";
 
     public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
         @Override
@@ -37,17 +58,28 @@ public class TaxaAdjacencyMatrix {
             int nTaxa = treeModel.getTaxonCount();
             int[][] adjacencyMatrix = new int[nTaxa][nTaxa];
             double[][] sharedBorder = new double[nTaxa][nTaxa];
+            double[] area = new double[nTaxa];
+            double[] perimeter = new double[nTaxa];
             for (XMLObject cxo : xo.getAllChildren(ADJACENCY)) {
-                List<Taxon> taxa = cxo.getAllChildren(Taxon.class);
-                int i = treeModel.getTaxonIndex(taxa.get(0));
-                int j = treeModel.getTaxonIndex(taxa.get(1));
+                List<XMLObject> taxa = cxo.getAllChildren(TAXON_DATA);
+                Taxon taxon0 = (Taxon) taxa.get(0).getChild(Taxon.class);
+                Taxon taxon1 = (Taxon) taxa.get(1).getChild(Taxon.class);
+
+                int i = treeModel.getTaxonIndex(taxon0);
+                int j = treeModel.getTaxonIndex(taxon1);
                 adjacencyMatrix[i][j] = 1;
                 adjacencyMatrix[j][i] = 1;
                 double sb = cxo.getAttribute(SHARED_BORDER, 0.0);
                 sharedBorder[i][j] = sb;
                 sharedBorder[j][i] = sb;
+
+                area[i] = taxa.get(0).getAttribute(AREA, 0.0);
+                area[j] = taxa.get(1).getAttribute(AREA, 0.0);
+                perimeter[i] = taxa.get(0).getAttribute(PERIMETER, 0.0);
+                perimeter[j] = taxa.get(1).getAttribute(PERIMETER, 0.0);
+
             }
-            return new TaxaAdjacencyMatrix(treeModel, adjacencyMatrix, sharedBorder);
+            return new TaxaAdjacencyMatrix(treeModel, adjacencyMatrix, sharedBorder, perimeter, area);
         }
 
         @Override
@@ -55,7 +87,11 @@ public class TaxaAdjacencyMatrix {
             return new XMLSyntaxRule[]{
                     new ElementRule(TreeModel.class),
                     new ElementRule(ADJACENCY, new XMLSyntaxRule[]{
-                            new ElementRule(Taxon.class, 2, 2),
+                            new ElementRule(TAXON_DATA, new XMLSyntaxRule[]{
+                                    new ElementRule(Taxon.class, 1, 1),
+                                    AttributeRule.newDoubleRule(PERIMETER),
+                                    AttributeRule.newDoubleRule(AREA)
+                            }, 2, 2),
                             AttributeRule.newDoubleRule(SHARED_BORDER, true)
                     }, 1, Integer.MAX_VALUE)
 
