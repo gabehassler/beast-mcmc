1e62b9a09905647338d82257b8ab6f30068bbb28
Gabe Hassler <ghassler@rand.org>
2023-03-30 10:25:51 -0700

    group sizes w/o threshold

--- src/dr/evomodel/territorydesign/TreeHeightSumStatistic.java
+++ src/dr/evomodel/territorydesign/TreeHeightSumStatistic.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 
 public class TreeHeightSumStatistic extends Statistic.Abstract {
     private final TreeModel treeModel;
-    private final Parameter height;
+    private final Parameter minimumSize;
     private final Parameter taxonValues;
     private final double[] groupCounts;
 
-    public TreeHeightSumStatistic(TreeModel treeModel, Parameter taxonValues, Parameter height) {
+    public TreeHeightSumStatistic(TreeModel treeModel, Parameter taxonValues, Parameter minimumSize) {
         super();
         this.treeModel = treeModel;
         this.taxonValues = taxonValues;
-        this.height = height;
+        this.minimumSize = minimumSize;
         this.groupCounts = new double[treeModel.getTaxonCount()];
     }
 
@@ -45,6 +45,17 @@ public class TreeHeightSumStatistic extends Statistic.Abstract {
         }
     }
 
+
+    private Accumulator mergeAccumulators(ArrayList<Accumulator> accumulators) {
+        int nChildren = 0;
+        double sum = 0;
+        for (Accumulator a : accumulators) {
+            nChildren += a.nChildren;
+            sum += a.sum;
+        }
+        return new Accumulator(nChildren, sum);
+    }
+
     private ArrayList<Accumulator> updateGroupCounts(NodeRef node) {
         ArrayList<Accumulator> accumulators = new ArrayList<>();
         if (treeModel.getChildCount(node) == 0) {
@@ -56,16 +67,31 @@ public class TreeHeightSumStatistic extends Statistic.Abstract {
         NodeRef c1 = treeModel.getChild(node, 1);
         ArrayList<Accumulator> a0 = updateGroupCounts(c0);
         ArrayList<Accumulator> a1 = updateGroupCounts(c1);
-
-        if (treeModel.getNodeHeight(node) < height.getParameterValue(0)) {
-            Accumulator b0 = a0.get(0);
-            Accumulator b1 = a1.get(0);
-            accumulators.add(new Accumulator(b0.nChildren + b1.nChildren, b0.sum + b1.sum));
-            return accumulators;
-        }
-
         accumulators.addAll(a0);
         accumulators.addAll(a1);
+
+        boolean merge = false;
+        if (a0.size() == 1 && a0.get(0).sum < minimumSize.getParameterValue(0)) {
+            merge = true;
+        }
+        if (a1.size() == 1 && a1.get(0).sum < minimumSize.getParameterValue(0)) {
+            merge = true;
+        }
+        if (merge) {
+            Accumulator merged = mergeAccumulators(accumulators);
+            accumulators.clear();
+            accumulators.add(merged);
+        }
+//
+//        if (treeModel.getNodeHeight(node) < height.getParameterValue(0)) {
+//            Accumulator b0 = a0.get(0);
+//            Accumulator b1 = a1.get(0);
+//            accumulators.add(new Accumulator(b0.nChildren + b1.nChildren, b0.sum + b1.sum));
+//            return accumulators;
+//        }
+//
+//        accumulators.addAll(a0);
+//        accumulators.addAll(a1);
         return accumulators;
     }
 
@@ -82,24 +108,24 @@ public class TreeHeightSumStatistic extends Statistic.Abstract {
         return groupCounts[dim];
     }
 
-    private static final String HEIGHT = "height";
+    private static final String MIN_SIZE = "minimumSize";
     private static final String TRAIT = "traitParameter";
 
     public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
         @Override
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
             TreeModel treeModel = (TreeModel) xo.getChild(TreeModel.class);
-            Parameter height = (Parameter) xo.getChild(HEIGHT).getChild(Parameter.class);
+            Parameter minSize = (Parameter) xo.getChild(MIN_SIZE).getChild(Parameter.class);
             Parameter trait = (Parameter) xo.getChild(TRAIT).getChild(Parameter.class);
 
-            return new TreeHeightSumStatistic(treeModel, trait, height);
+            return new TreeHeightSumStatistic(treeModel, trait, minSize);
         }
 
         @Override
         public XMLSyntaxRule[] getSyntaxRules() {
             return new XMLSyntaxRule[]{
                     new ElementRule(TreeModel.class),
-                    new ElementRule(HEIGHT, new XMLSyntaxRule[]{
+                    new ElementRule(MIN_SIZE, new XMLSyntaxRule[]{
                             new ElementRule(Parameter.class)
                     }),
                     new ElementRule(TRAIT, new XMLSyntaxRule[]{
