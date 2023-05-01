0f69d9b0e449b806c700506b75ae0e18e305bc31
Gabe Hassler <ghassler@rand.org>
2023-05-01 14:32:11 -0700

    just return the exess numbers

--- src/dr/evomodel/territorydesign/TreeHeightSumStatistic.java
+++ src/dr/evomodel/territorydesign/TreeHeightSumStatistic.java
@@ -8,58 +8,57 @@ import java.util.ArrayList;
 
 public class TreeHeightSumStatistic extends Statistic.Abstract {
 
-    private final double[] groupCounts;
     private final TaxonGroupsProvider provider;
+    private final double maxSize;
 
 
-    public TreeHeightSumStatistic(TaxonGroupsProvider provider) {
+    public TreeHeightSumStatistic(TaxonGroupsProvider provider, double maxSize) {
         super();
         this.provider = provider;
-        this.groupCounts = new double[provider.getMaximumNumGroups()];
+        this.maxSize = maxSize;
     }
 
 
-    private void updateGroupCounts() {
+    private double getExcessGroupCounts() {
         ArrayList<TaxonGroupsProvider.Accumulator> accumulators = provider.getGroups();
-//        int offset = 0;
+        double sum = 0;
         for (int i = 0; i < accumulators.size(); i++) {
             TaxonGroupsProvider.Accumulator accumulator = accumulators.get(i);
-            groupCounts[i] = accumulator.sum;
-//            for (int j = offset; j < accumulator.nChildren + offset; j++) {
-//                groupCounts[j] = accumulator.sum;
-//            }
-//            offset += accumulator.nChildren;
+            if (accumulator.sum > maxSize) {
+                sum += accumulator.sum - maxSize;
+            }
         }
-        for (int i = accumulators.size(); i < groupCounts.length; i++) {
-            groupCounts[i] = 0;
-        }
+        return sum;
     }
 
 
     @Override
     public int getDimension() {
-        return groupCounts.length;
+        return 1;
     }
 
     @Override
     public double getStatisticValue(int dim) {
-        if (dim == 0) {
-            updateGroupCounts();
-        }
-        return groupCounts[dim];
+        return getExcessGroupCounts();
     }
+
+
+    private static final String GROUP_SUM_STATISTIC = "groupSumStatistic";
+    private static final String MAX_SIZE = "maxSize";
 
     public static AbstractXMLObjectParser PARSER = new AbstractXMLObjectParser() {
         @Override
         public Object parseXMLObject(XMLObject xo) throws XMLParseException {
             TaxonGroupsProvider taxonGroupsProvider = (TaxonGroupsProvider) xo.getChild(TaxonGroupsProvider.class);
-            return new TreeHeightSumStatistic(taxonGroupsProvider);
+            double maxSize = xo.getDoubleAttribute(MAX_SIZE);
+            return new TreeHeightSumStatistic(taxonGroupsProvider, maxSize);
         }
 
         @Override
         public XMLSyntaxRule[] getSyntaxRules() {
             return new XMLSyntaxRule[]{
-                    new ElementRule(TaxonGroupsProvider.class)
+                    new ElementRule(TaxonGroupsProvider.class),
+                    AttributeRule.newDoubleRule(MAX_SIZE)
             };
         }
 
@@ -76,7 +75,7 @@ public class TreeHeightSumStatistic extends Statistic.Abstract {
 
         @Override
         public String getParserName() {
-            return "groupSumStatistic";
+            return GROUP_SUM_STATISTIC;
         }
     };
 }
